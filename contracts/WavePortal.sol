52dd437792ba911787e9a61441196fdc7f33f3b8
Tim Delle <tidelle@goldbeck.biz>
2022-01-24 20:09:47 +0100

    [main] Using env files now 🤘

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+contract WavePortal {
+  uint256 totalWaves;
+
+  constructor() {}
+
+  function wave() public {
+    totalWaves += 1;
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    return totalWaves;
+  }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_RINKEBY_API_KEY,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
